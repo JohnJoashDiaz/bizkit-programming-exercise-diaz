d475661f69ba8e748f2948cc0337c9168b504571
Joash Diaz <johnjoashcolangodiaz@gmail.com>
2022-09-26 01:11:12 +0800

    Finalized Search

--- phasebook/match.py
+++ phasebook/match.py
@@ -47,41 +47,6 @@ def is_match(fave_numbers_1, fave_numbers_2):
             else:
                 print ("Element", fave_numbers_2[index], " Cannot Be found") 
                 return False
-                    #print ("value at fave_num 2 is ", str(fave_numbers_2[index_2]))
-                    #print("Number Found at index: " + str(mid))
-                    #print ("number found at index no. ", str(fave_numbers_1[mid]))  
-                
-
-
-
-    
-    
-    
-    
-    
-    #print ("last is ", last)
-    #index = -1
-    #index_2 = 0
-    #while (first <= last) and (index == -1):
-    #    mid = (first + last) //2
-    #    print (mid)
-    #    if fave_numbers_1[mid] == fave_numbers_2[index_2]:
-    #        index = mid 
-    #    else:
-    #        if fave_numbers_2[index_2] < fave_numbers_1[mid]:
-    #            last = mid - 1 
-    #        else:
-    #            first= mid +1
-    #    index_2 = index_2 + 1
-    #return True
-    
-    #linear search algo
-    #for number in fave_numbers_2:
-    #    if number not in fave_numbers_1:
-    #        return False
-
-
-
    
 
     
--- phasebook/search.py
+++ phasebook/search.py
@@ -24,7 +24,68 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
+    args = request.args
+    print (USERS)
+    id = args.get('id')
+    name = args.get('name')
+    age = args.get('age')
+    occupation = args.get('occupation')
+    result = []
 
-    # Implement search here!
+    if request.args == {}:
+        print("no parameters, displaying all users")
+        result = USERS
+    if 'id' in request.args:
+        print ("searching for id matches")
+        print (args)
+        index = 0
+        for i in USERS:
+            if id in USERS[index]['id']:
+                result.append(USERS[index])
+                print("appending", USERS[index])
+                index+=1
+            else:
+                index+=1
+    if 'name' in request.args:
+        print ("searching for name matches")
+        index = 0
+        for i in USERS:
+            if name in USERS[index]['name'] or name.capitalize() in USERS[index]['name']:
+                result.append(USERS[index])
+                print("appending", USERS[index])
+                index+=1
+            else:
+                index+=1
+    if 'age' in request.args:
+        print ("scanning for age matches")
+        index = 0
+        age_mid = int(age)
+        age_low = age_mid -1
+        age_high = age_mid +1
+        for i in USERS:
+            if age_low == USERS[index]['age']:
+                result.append(USERS[index])
+                index+=1 
+            elif age_mid == USERS[index]['age']:
+                result.append(USERS[index])
+                index+=1 
+            elif age_high == USERS[index]['age']:
+                result.append(USERS[index])
+                index+=1
+            else:
+                index+=1     
+    if 'occupation' in request.args:
+        print ("searching for matches in occupation")
+        index = 0
+        for i in USERS:
+            if occupation in USERS[index]['occupation'] or occupation.capitalize() in USERS[index]['occupation']:
+                result.append(USERS[index])
+                index+=1
+            else:
+                index+=1
+    result = [i for n, i in enumerate(result) if i not in result[:n]] 
+    return result
+    
+    
+    
 
-    return USERS
